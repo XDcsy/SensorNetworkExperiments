d93fa66e022a8e839a5c6cc0505f62d68a108bd6
neEverett <shengyang.cheng@outlook.com>
2017-03-11 13:53:34 +0800

    Add code and results.

--- Sensor Network/02.connectivity and coverage under COMPOW/compow.m
+++ Sensor Network/02.connectivity and coverage under COMPOW/compow.m
@@ -1,13 +1,15 @@
 
 function[] = compow()
-    n = 50;
-	rMax = 0.5;
+    n = 10;
+	rMax = 0.8;
 	graph = spread_nodes(n);
 	creat2Tables(graph, n, rMax);
-	draw();
+	draw(1, graph);
 	minR = reduceR();  
-	draw();
-	countCoverage();
+	draw(2, graph, minR);
+	ratio = countCoverage(graph, minR, n);
+	disp(num2str(ratio));
+    clear global;
 end
 
 function[graph] = spread_nodes(n)
@@ -17,41 +19,77 @@ end
 function[] = creat2Tables(graph, n, rMax)
     global connectionTable distanceTable;
 	connectionTable(1 : n) = 0;       %store the counts of each node's neighbour
-	distanceTable(n*(n-1)/2, 3) = 0;  %1:store distance, 2&3:nodes
-	for i = 1 : n
+	distanceTable(1:n*(n-1)/2, 1:3) = -1;  %1:store distance, 2&3:nodes
+	num = 1;
+    for i = 1 : n
         for j = i + 1 : n
-		    num = 1;
 		    distance = sqrt( (graph(i,1)-graph(j,1))^2 + (graph(i,2)-graph(j,2))^2 );
-			if distance < rMax
+			if distance <= rMax && distance >= 0
 			    distanceTable(num,1) = distance;
 				distanceTable(num,2) = i;
 				distanceTable(num,3) = j;
 			    connectionTable(i) = connectionTable(i) + 1;
 				connectionTable(j) = connectionTable(j) + 1;
-			end
+            end
+            num = num + 1;
 		end
-	end
+    end
+    distanceTable = sortrows(distanceTable,-1);
+    if ~isempty(find(connectionTable == 0,1));
+        error('The graph is not connected even at max communication range.');
+    end
 end
 
 function[minR] = reduceR()
-    global distanceTable;
-	sortrows(distanceTable,-1);
+    global distanceTable connectionTable;
 	i = 1;
 	while 1
 	    node1 = distanceTable(i,2);
 		node2 = distanceTable(i,3);
 		connectionTable(node1) = connectionTable(node1) - 1;
 		connectionTable(node2) = connectionTable(node2) - 1;
-		if connectionTable(node1) <=0 | connectionTable(node2) <=0
+		if connectionTable(node1) <=0 || connectionTable(node2) <=0
 		    minR = distanceTable(i-1,1);
 			break;
 		end
 		i = i + 1;
 	end
 end
-%creat graph
-%at max communication range, check whether the graph is connected
-%creat a table that includes distance and the node's connected nodes
-%sort the table by distance
-%form the max distance, remove the connected line and check the num of the 2 nodes' connected nodes. Unitl find a 0.
-%creat 100*100 dots and check each one's distance form all nodes. Record if it's covered.
+
+function[] = draw(n, graph, minR)
+    global distanceTable
+    x = graph(:,1);
+    y = graph(:,2);
+	subplot(2,1,n);
+    plot(x,y,'ro');
+	i = 1;
+	if n==2
+        while distanceTable(i,1) > minR
+            i = i+1;
+        end
+    end
+	node1 = distanceTable(i,2);
+	node2 = distanceTable(i,3);
+    while node1 ~=-1
+        hold on; 
+		line ([x(node1) x(node2)],[y(node1) y(node2)]);
+		i = i+1;
+		node1 = distanceTable(i,2);
+	    node2 = distanceTable(i,3);
+    end
+end
+
+function[ratio] = countCoverage(graph, minR, n)
+    coveredNodes = 0;
+	for x = 0 : 0.01 : 1
+	    for y = 0 : 0.01 : 1
+		    for node = 1 : n
+			    if sqrt( (graph(node,1)-x)^2 + (graph(node,2)-y)^2 ) < minR
+				    coveredNodes = coveredNodes + 1;
+					break;
+				end
+			end
+		end
+	end
+	ratio = coveredNodes/(101*101);
+end
