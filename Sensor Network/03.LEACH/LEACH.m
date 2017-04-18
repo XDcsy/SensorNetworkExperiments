8b70209d66d766247261793585cc6112e862fc18
neEverett <shengyang.cheng@outlook.com>
2017-04-19 01:24:01 +0800

    Upload code

---
+++ Sensor Network/03.LEACH/LEACH.m
@@ -0,0 +1,65 @@
+clear;
+energyLeft = 1;
+round = 0;
+num = 500;  %节点数目
+pow = 10000;  %总能量
+P = 0.1;
+range = 1;  %节点接收此范围内的簇头广播
+times = 10;  %每轮发送数据次数
+sendCost = 2;  %节点间发送一次数据的能量消耗
+receiveCost = 1;  %节点间接收一次数据的能量消耗
+broadcastCost = 4;  %簇头节点向周围广播的能量消耗
+remoteRransmitCost = 100;  %簇头节点远距离通信的能量消耗
+
+for i = 1 : num
+    nodes(i) = node(pow);
+end
+while energyLeft
+    if mod(round,1/P) == 0 && round ~= 0  %在1/p轮后，所有节点再次可以被选为簇头
+        for i = 1 : num
+            nodes(i).wasElected = 0;
+        end
+    end
+    
+    T = P/(1-P*mod(round,1/P));
+    headnodes = [];
+    nothaednodes = [];
+    for i = 1 : num  %每一轮开始时清空上一轮的簇头记录
+        nodes(i).clearHead();
+    end
+    for i = 1 : num  %选举簇头
+        nodes(i).elect(T);
+        if nodes(i).ishead
+            headnodes = [headnodes nodes(i)];
+        else
+            nothaednodes = [nothaednodes nodes(i)];
+        end
+    end
+    for i = 1 : length(headnodes)  %簇头广播
+        headnodes(i).send(broadcastCost);
+    end
+    for i = 1 : num  %各节点接收广播信号，找到所属簇头
+        nodes(i).findhead(headnodes, range, receiveCost)
+    end
+    for i = 1 : length(nothaednodes)  %非簇头节点通知各自簇头，各簇头接收
+        nothaednodes(i).send(sendCost);
+        nothaednodes(i).head.recive(receiveCost);
+    end
+    for i = 1 : times  %完成一轮的数据收发
+        for j = 1 : length(nothaednodes)  %非簇头节点发送，各簇头接收
+            nothaednodes(j).send(sendCost);
+            nothaednodes(j).head.recive(receiveCost);
+        end
+        for j = 1 : length(headnodes)  %簇头节点远程发送
+            headnodes(j).send(remoteRransmitCost);
+        end
+    end
+    for i = 1 : num  %检查各节点剩余能量
+        if nodes(i).power <= 0
+            energyLeft = 0;
+            break;
+        end
+    end
+    round = round + 1;
+end
+disp(round);
---
+++ Sensor Network/03.LEACH/node.m
@@ -0,0 +1,57 @@
+classdef node < handle
+    %UNTITLED3 此处显示有关此类的摘要
+    %   此处显示详细说明
+    
+    properties
+        coordinate;
+        randnum;
+        ishead;
+        wasElected;
+        head;
+        power;
+    end
+    
+    methods
+        function[obj] = node(pow)
+            obj.coordinate = rand(1,2);
+            obj.randnum = rand();
+            obj.ishead = 0;
+            obj.wasElected = 0;
+            obj.power = pow;
+        end
+        function[] = clearHead(obj)
+            obj.head = [];
+        end
+        function[] = elect(obj, T)
+            if obj.ishead == 1  %清空上一轮的簇头
+                obj.ishead = 0;
+                return;
+            end
+            if ((obj.wasElected == 0) && (obj.randnum < T))  %满足被选为簇头的条件
+                obj.ishead = 1;
+                obj.wasElected = 1;
+            end
+        end
+        function[] = findhead(obj, headnodes, range, cost)
+            distance = 2;
+            for i = 1 : length(headnodes)
+                tmp = sqrt(sum((obj.coordinate - headnodes(i).coordinate).^2));
+                if tmp < distance
+                    distance = tmp;
+                    obj.head = headnodes(i);
+                end
+                if distance < range  %距离小于通信距离时，节点接收到信号
+                    obj.recive(cost);
+                end
+            end
+        end
+        function[] = send(obj, cost)
+            obj.power = obj.power - cost;
+        end
+        function[] = recive(obj, cost)
+            obj.power = obj.power - cost;
+        end
+    end
+    
+end
+
